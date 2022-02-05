db74d563162cc1118db594ef7034171830c21eb9
Jashan01 <jashanbindra@hotmail.com>
2022-02-06 01:40:44 +0530

    Pages added all

--- lib/enums.dart
+++ lib/enums.dart
@@ -1,1 +1,1 @@
-enum MenuState { home, favourite, message, profile }
+enum MenuState { home, resources, mentor,story, profile }
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/widgets.dart';
+import 'package:shop_app/resources/resources_screen.dart';
 import 'package:shop_app/screens/cart/cart_screen.dart';
 import 'package:shop_app/screens/complete_profile/complete_profile_screen.dart';
 import 'package:shop_app/screens/details/details_screen.dart';
 import 'package:shop_app/screens/forgot_password/forgot_password_screen.dart';
 import 'package:shop_app/screens/home/home_screen.dart';
 import 'package:shop_app/screens/login_success/login_success_screen.dart';
+import 'package:shop_app/screens/mentor/mentor_screen.dart';
 import 'package:shop_app/screens/otp/otp_screen.dart';
 import 'package:shop_app/screens/profile/profile_screen.dart';
 import 'package:shop_app/screens/sign_in/sign_in_screen.dart';
 import 'package:shop_app/screens/splash/splash_screen.dart';
+import 'package:shop_app/story/story_screen.dart';
 
 import 'screens/sign_up/sign_up_screen.dart';
 
@@ -26,4 +29,7 @@ final Map<String, WidgetBuilder> routes = {
   DetailsScreen.routeName: (context) => DetailsScreen(),
   CartScreen.routeName: (context) => CartScreen(),
   ProfileScreen.routeName: (context) => ProfileScreen(),
+  MentorScreen.routeName: (context) => MentorScreen(),
+  ResourcesScreen.routeName: (context) => ResourcesScreen(),
+  StoryScreen.routeName: (context) => StoryScreen(),
 };
